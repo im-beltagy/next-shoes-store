304d59a19e23f665d07ecb85c547dc666fad6c0a
im-beltagy <imbeltagy@gmail.com>
2024-07-08 00:49:15 +0300

    refactor: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,7 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/layout/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/view/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
