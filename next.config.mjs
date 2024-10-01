bf25ea104bc3580e571ad3844f1e81ec540c0f00
im-beltagy <imbeltagy@gmail.com>
2024-10-01 21:00:46 +0300

    chor: image configs

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["i.pravatar.cc"],
+    remotePatterns: [{ protocol: "https", hostname: "**" }],
   },
 };
 
